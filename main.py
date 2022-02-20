0b1895f6b32471618f31cf634de779cd906b4ba2
Plamen Atanasov <pl.atanasov89@gmail.com>
2022-02-20 11:30:57 +0200

    final touches 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from PIL import Image
-from sklearn.cluster import KMeans
+# from sklearn.cluster import KMeans
 from flask import Flask, render_template, redirect, url_for, request
 from werkzeug.utils import secure_filename
 import os
@@ -14,24 +14,22 @@ def rgb2hex(r, g, b):
 hex_list = []
 
 # it works damn slow, so I used ColorTheaf instead
+# def get_colors_kmeans(img):
+#     hex_list.clear()
+#     img_array = np.asarray(img)
+#     clt = KMeans(n_clusters=10)
+#     clt.fit(img_array.reshape(-1, 3))
+#     top10 = clt.cluster_centers_
+#     print(top10)
+#     top10_list = top10.tolist()
 
+#     for i in range(len(top10_list)):
 
-def get_colors_kmeans(img):
-    hex_list.clear()
-    img_array = np.asarray(img)
-    clt = KMeans(n_clusters=10)
-    clt.fit(img_array.reshape(-1, 3))
-    top10 = clt.cluster_centers_
-    print(top10)
-    top10_list = top10.tolist()
-
-    for i in range(len(top10_list)):
-
-        col = rgb2hex(int(top10_list[i][0]), int(
-            top10_list[i][1]), int(top10_list[i][2]))
-        hex_list.append(col)
-        print(hex_list)
-    return hex_list
+#         col = rgb2hex(int(top10_list[i][0]), int(
+#             top10_list[i][1]), int(top10_list[i][2]))
+#         hex_list.append(col)
+#         print(hex_list)
+#     return hex_list
 
 
 def get_colors(img):
